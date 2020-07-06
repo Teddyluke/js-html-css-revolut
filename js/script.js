b5e6f7b52a5d4f12724f1c55b22a49beb67aecd1
Luca Maglie <luca.maglie2@gmail.com>
2020-07-06 17:30:20 +0200

    ok dropdown menù

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
-  // effetti mouse in & out 
+  // effetti mouse in & out
   $(".top-header a").mouseenter(function () {
-    $(this).css("color","black")
-  })
+    $(this).css("color","black");
+  });
   $(".top-header a").mouseout(function () {
-    $(this).css("color","grey")
-  })
+    $(this).css("color","grey");
+  });
   $(".nav-menu a").mouseenter(function () {
-    $(this).css("color","black")
-  })
+    $(this).css("color","black");
+  });
   $(".nav-menu a").mouseout(function () {
-    $(this).css("color","grey")
-  })
+    $(this).css("color","grey");
+  });
   $(".download-app-btn").mouseenter(function () {
     $(this).fadeTo( "slow", 0.7, function () {
-    })
-  })
+    });
+  });
   $(".download-app-btn").mouseout(function () {
     $(this).fadeTo( "slow", 1, function () {
-    })
-  })
+    });
+  });
+ // regole per dropdown menu
+  $(".nav-menu li").mouseenter(function () {
+    $(this).children(".dropdown").addClass("show").removeClass("hidden");
+  });
 
-
+  $(".nav-menu li").mouseleave(function () {
+    $(this).children(".dropdown").addClass("hidden").removeClass("show");
+  });
 
 });
